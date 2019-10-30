12f20e0bde5f176dee5e29e327fdb3e36beb1528
sdusza1 <sebastian.dusza@circlekeurope.com>
2019-10-30 21:05:21 +0100

    Push to s3 + deployment

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,24 +4,24 @@ source ./ci/config.sh
 
 export VERSION=$1
 export IMAGE_URL="$ECR_REGISTRY/$2"
-APP_SPEC_FILE="ci/appspec-$VERSION.json"
+APP_SPEC_FILE="appspec-$VERSION.json"
 
 
-# parse task definition
+# parse & publish task definition
 envsubst < "ci/ecs-task.json.tpl" > "ci/ecs-task.json"
-
 export TASK_DEFINITION_ARN=`aws ecs register-task-definition --cli-input-json file://ci/ecs-task.json --region eu-west-1 | jq -r .taskDefinition.taskDefinitionArn`
 echo "TASK_DEFINITION_ARN=$TASK_DEFINITION_ARN"
 
-# parse deployment definition
-envsubst < "ci/appspec.json.tpl" > $APP_SPEC_FILE
+# parse & publish deployment definition
+envsubst < "ci/appspec.json.tpl" > "ci/$APP_SPEC_FILE"
+aws s3 cp "ci/$APP_SPEC_FILE" s3://newweb-experiment-deployment
 
-
-#aws deploy create-deployment \
-#    --application-name newweb-experiment \
-#    --deployment-group-name newweb-experiment-deploy \
-#    --description "Deployment" \
-#    --s3-location bucket=newweb-experiment-deployment,bundleType=JSON,key=appspec.json
+# deploy
+aws deploy create-deployment \
+    --application-name newweb-experiment \
+    --deployment-group-name newweb-experiment-deploy \
+    --description "Deployment VERSION" \
+    --s3-location bucket=newweb-experiment-deployment,bundleType=JSON,key=$APP_SPEC_FILE
 
 
 
