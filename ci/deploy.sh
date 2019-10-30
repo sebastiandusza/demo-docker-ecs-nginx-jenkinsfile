76556e56c996c02bef633b4b4f3f282f6aa7bfad
sdusza1 <sebastian.dusza@circlekeurope.com>
2019-10-30 22:13:13 +0100

    v6

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,7 +9,6 @@ APP_SPEC_FILE="appspec-$VERSION.json"
 # parse & publish task definition
 envsubst < "ci/ecs-task.json.tpl" > "ci/ecs-task.json"
 export TASK_DEFINITION_ARN=`aws ecs register-task-definition --cli-input-json file://ci/ecs-task.json --region eu-west-1 | jq -r .taskDefinition.taskDefinitionArn`
-echo "TASK_DEFINITION_ARN=$TASK_DEFINITION_ARN"
 
 # parse & publish deployment definition
 envsubst < "ci/appspec.json.tpl" > "ci/$APP_SPEC_FILE"
@@ -19,7 +18,7 @@ aws s3 cp "ci/$APP_SPEC_FILE" s3://newweb-experiment-deployment
 aws deploy create-deployment \
     --application-name newweb-experiment \
     --deployment-group-name newweb-experiment-deploy \
-    --description "Deployment VERSION" \
+    --description "Deployment $VERSION" \
     --s3-location bucket=newweb-experiment-deployment,bundleType=JSON,key=$APP_SPEC_FILE
 
 
