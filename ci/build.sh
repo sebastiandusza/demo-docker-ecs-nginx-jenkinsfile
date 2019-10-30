3d070c90eb817c39aafeb48d84e5e11509243c34
sdusza1 <sebastian.dusza@circlekeurope.com>
2019-10-30 20:40:43 +0100

    Build script

---
+++ ci/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+source ./config.sh
+
+export VERSION=$1
+
+docker build --pull -t "experiment-nginx:latest" -f ./ci/Dockerfile .
+docker tag "experiment-nginx:latest" "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
+$(aws ecr get-login --region "eu-west-1" --no-include-email)
+docker push "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
