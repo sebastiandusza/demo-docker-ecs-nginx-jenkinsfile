04accfe0af97590fd18160e2f6d21deef8af1eda
sdusza1 <sebastian.dusza@circlekeurope.com>
2019-10-30 20:49:13 +0100

    Bug fixes

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-source ./config.sh
+source ./ci/config.sh
 
 export VERSION=$1
 
@@ -8,3 +8,5 @@ docker build --pull -t "experiment-nginx:latest" -f ./ci/Dockerfile .
 docker tag "experiment-nginx:latest" "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
 $(aws ecr get-login --region "eu-west-1" --no-include-email)
 docker push "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
+
+echo "Docker image experiment-nginx:$VERSION prepared and pushed"
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 set -e
-source ./config.sh
+source ./ci/config.sh
 
 export VERSION=$1
 export IMAGE_URL="$ECR_REGISTRY/$2"
-APP_SPEC_FILE="appspec-$VERSION.json"
+APP_SPEC_FILE="ci/appspec-$VERSION.json"
 
 # parse deployment definition
-envsubst < "appspec.json.tpl" > $APP_SPEC_FILE
+envsubst < "ci/appspec.json.tpl" > $APP_SPEC_FILE
 
 # parse task definition
-envsubst < "ecs-task.json.tpl" > "ecs-task.json"
+envsubst < "ci/ecs-task.json.tpl" > "ci/ecs-task.json"
 
 aws ecs register-task-definition \
-    --cli-input-json file://ecs-task.json \
+    --cli-input-json file://ci/ecs-task.json \
     --region eu-west-1
 
 #aws deploy create-deployment \
